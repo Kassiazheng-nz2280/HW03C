7c873e094a6139d759d2bb1a2beab8a8dbbcd815
Kassia Zheng <nz2280@nyu.edu>
2023-09-26 12:29:53 -0400

    final one

--- sketch.js
+++ sketch.js
@@ -3,4 +3,26 @@ function setup() {
 }
 
 function draw() {
+  randomSeed(100);
+  for (let counter = 0; counter < 1052; counter += 1) {
+
+    let x1 = random(0, windowWidth);
+    let y1 = random(0, windowHeight);
+    let x0 = random(0, windowWidth);
+    let y0 = random(0, windowHeight);
+    
+    stroke('black');
+    strokeWeight(4);
+    line(x1, y1, x0, y0);
+
+    stroke('pink');
+    fill('pink');
+    strokeWeight(1);
+    ellipse(x1, y1, 10);
+    ellipse(x0, y0, 10);
+
+    
+
+    
+  }
 }
